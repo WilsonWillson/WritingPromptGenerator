4d22fecfe50c7856ff53b1257e229e58b504fbe9
WilsonWillson <Wilsonjc@smu.edu>
2014-12-11 03:07:55 -0600

    Finished all the subjects
    added new ones too

--- src/com/example/writingpromptgenerator/GetRandomSubject.java
+++ src/com/example/writingpromptgenerator/GetRandomSubject.java
@@ -5,10 +5,14 @@ import java.util.Random;
 public class GetRandomSubject {
 		
 	static String heroCompare = "Hero";
-	static String[] hero = {"soldier","firefighter", "police officer"};
+	static String[] hero = {"soldier","firefighter", "police officer", "Mahatma Gandhi", "Nelson Mandela",
+			"Martin Luther King Jr.", "John Rambo", "Forrest Gump", "Sherlock Holmes", "Frodo Baggins",
+			"Rocky Balboa", "Han Solo", "Indiana Jones", "Spartacus", };
 	
 	static String villianCompare = "Villian";
-	static String[] villian = {"evil Mastermind","dictator",};
+	static String[] villian = {"evil Mastermind","dictator", "overlord", "tyrant", "Hitler",
+	"evil genius", "Attila the Hun", "Vlad Dracula", "Jack the Ripper", "Buffalo Bill", "Hannibal lecter",
+	"Ivan Drago","Norman Bates", "The Wicked Witch of the West", "Freddy Krueger" };
 
 	static String professionCompare = "Profession";
 	static String[] profession = {"baker","doctor","dentist", "nurse", "pharmacist", "physician", "software developer",
@@ -20,25 +24,54 @@ public class GetRandomSubject {
 		"consutrtion worker"};
 	
 	static String historicalFigureCompare = "Historical Figure";
-	static String[] historicalFigure = {"George Washington","Abe Lincoln",};
+	static String[] historicalFigure = {"George Washington","Abe Lincoln", "Napoleon", "William Shakespeare", "Aristotle",
+		"Alexander the Great", "Thomas Jefferson", " Henry VIII of England", "Charlies Darwin", 
+		"Elizabeth I of England", "Karl Marx", "Julius Caesar", "Martin Luther", "Queen Victoria", "Julius Caesar",
+		"Martin Luther", "Joseph Stalin", "Albert Einstein", "Christopher Columbus", "Isaac Newton", "Theodore Roosevelt",
+		"Mozart", "Plato", "Beethoven", "Leonardo da Vinci", "Ulysses S. Grant", "Benjamin Franklin", "Charles Dickens",
+		"Ronald Reagan", "Genghis Khan", "Thomas Edison", "George W. Bush", "Franklin D. Roosevelt", "Sigmund Freud", 
+		"Woodrow Wilson", "Mark Twain", "Edgar Allan Poe", "Andrew Jackson", "Elvis Presley", "John F. Kennedy", "Oscar Wilde",
+		"King Arthur", "Michelangelo", "Nikola Tesla", "Joan of Arc", "Otto von Bismarck"};
     
-	static String spaceCompare = "Space";
-	static String[] space = {"astronaut","alien", ""};
+	static String spaceCompare = "Space Fictional/Real";
+	static String[] space = {"astronaut","alien", "little gray men", "scientist", "jedi", "stormtroppers", "bith", "cerean", 
+		"corellian", "ewok", "gamorrean", "gungan", "hutt", "jawa", "kel dor", "rodian", "wookie", "vulcan", "andorian", "klingon",
+		"Neil Armstrong", "Captain James T. Kirk", "Luke Skywalker", "Han Solo"};
 	
 	static String angelAndDemonsCompare = "Angels and Demons";
-	static String[] angelAndDemons = {"Angel","Demon",};
+	static String[] angelAndDemons = {"angel","demon","fallen angel", "familiar", "jinn", "succubus",
+		"imp", "poltergeist", "ghost", "spirt", "seraphim", "virtues", "principalities", "archangels",
+		"devil", "dominions"};
 	
 	static String animalCompare = "Animal";
-	static String[] animal = {"duck","elephant",};
+	static String[] animal = {"duck","elephant","tiger", "jaguar", "monkey", "dog", "lion", "weasel", 
+		"deer", "octopus", "reindeer", "komodo dragon", "snake", "yak", "cheetah", "fish", "mole",
+		"wolf", "zebra", "bear", "panda", "centipede", "quail", "gecko", "lizard", "dolphin", 
+		"squirrel", "squid", "spider", "ants", "baboons", "sea lions", "orangutans", "pigeon", "crow",
+		"donkey", "cow", "turkey","sheep", "racoon", "horse", "falcon", "rat", "owl", "whale", 
+		"parrot", "pig", "chimpanzee"};
 
 	static String mythicalCreatureCompare = "Mythical Creatures";
-	static String[] mythicalCreature = {"minotaur","pheonix",};
+	static String[] mythicalCreature = {"minotaur","pheonix","dragon", "unicorn", "pegasus", "sphinx",
+		"cerberus", "minotaur", "gargoyle", "ogre", "troll", "leviathan", "griffin", "leviathan", 
+		"hydra", "centaur", "ghoul", "griffin", "harpy", "hippogriff", "chimera", "mermaid", "fairy",
+		"medusa", "vampire", "cyclops", "basilisk", "golem", "imp", "sirens", "shapeshifters", "wisp"};
 	
 	static String medevilJobCompare = "Medevil Job";
-	static String[] medevilJob = {"blacksmith","forger",};
+	static String[] medevilJob = {"blacksmith","forger","apothecary", "artist", "armorer", "candlemaker", 
+		"carpenter", "jester", "knight", "king", "prince", "painter ", "scribe", "serf", "vassal", 
+		"nobel", "servant", "peasant", "assassin", "preist", "tailor", "falconer", "stonemason", 
+		"miller", "fishmonger", "theif"};
 	
-	static String prehistoricSubjectCompare = "Prehistoric Subject";
-	static String[] prehistoricSubject = {"caveman","dinosaur",};
+	static String comicBookCompare = "Comic Book Characters";
+	static String[] comicBook = {"Abe Sapien", "Hellboy", "Apocalypse", "Iron Fist", "Static Shock", 
+		"Dr. Manhattan", "Galactus", "Bane", "Venom", "Silver Surfer", "Batwoman", "Deadpool",
+		"Ultron", "Thanos", "Rorschach", "Hawkeye", "Nick Fury", "Beast", "Mr. Fantastic", 
+		"Lex Luthor", "V", "Doctor Strange", "Green Goblin", "Fone Bone", "Robin", "Storm", "Hulk",
+		"Thor", "Swamp Thing", "Jean Grey", "Judge Dredd", "The Flash", "Iron Man", "The Thing", 
+		"Green Latern", "Dr. Doom", "Professor X", "John Constantine", "Magneto", "Wolverine", 
+		"Captain America", "Morpheus", "Wonder Woman", "Daredevil", "The Joker", "Superman", 
+		"Batman", "Spider-Man", "The Human Torch"};
 
 	public static String RandomSubject(String subjectTopic){
 		 String subject = " ";
@@ -77,10 +110,6 @@ public class GetRandomSubject {
 			 	}
 		 	if(subjectTopic.equals(medevilJobCompare)){
 			 	 subject = (medevilJob[new Random().nextInt(medevilJob.length)]);
-			 		return subject;
-			 	}
-		 	if(subjectTopic.equals(prehistoricSubjectCompare)){
-			 	 subject = (prehistoricSubject[new Random().nextInt(prehistoricSubject.length)]);
 			 		return subject;
 			 	}
 		return null;	
